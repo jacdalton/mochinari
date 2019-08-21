08ba7136a7422fd917722eed5f2fb81c6f667a35
mitchshiotani <45535086+mitchshiotani@users.noreply.github.com>
2019-08-21 15:05:06 +0900

    Update snack_images_controller.rb

--- app/controllers/snack_images_controller.rb
+++ app/controllers/snack_images_controller.rb
@@ -1,4 +1,5 @@
 class SnackImagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_snack_image, only: [:show, :edit, :update, :destroy]
   before_action :set_snack, only: [:new, :create, :edit, :update, :destroy]
   def index
