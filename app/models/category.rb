79a9353d880420d6513759ea3fad49dc7cd35d27
Mitch Shiotani <mitch.shiotani1@gmail.com>
2019-08-21 10:42:52 +0900

    added validations and relationships for category, snack_image, and user

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+  has_many :snacks
+  has_many :snack_images, through: :snacks
+
+  validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :description, presence: true
+  validates :photo_url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :trails
+  has_many :snack_ratings
+  has_many :snack_images
+  has_many :snacks
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :username, presence: true
 end
