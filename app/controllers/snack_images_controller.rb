0733f8091779240a1fe0da8e686f0eff3b6e1ece
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-21 14:57:52 +0900

    Added basic CRUD actions for SnacksImages controller and fixed typo in Snacks controller

--- app/controllers/snack_images_controller.rb
+++ app/controllers/snack_images_controller.rb
@@ -1,5 +1,6 @@
 class SnackImagesController < ApplicationController
   before_action :set_snack_image, only: [:show, :edit, :update, :destroy]
+  before_action :set_snack, only: [:new, :create, :edit, :update, :destroy]
   def index
     # once we implement Pundit this should probably change
     @snack_images = SnackImage.all
@@ -14,16 +15,28 @@ class SnackImagesController < ApplicationController
 
   def create
     @snack_image = SnackImage.new(snack_image_params)
-    @snack_image.snack = Snack.find(params[:snack_id])
+    @snack_image.snack = @snack
+    if @snack_image.save
+      redirect_to snack_path(@snack)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @snack_image.update(snack_image_params)
+      redirect_to snack_path(@snack)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @snack_image.destroy
+    redirect_to snack_path(@snack)
   end
 
   private
@@ -33,7 +46,10 @@ class SnackImagesController < ApplicationController
   end
 
   def set_snack_image
-    @snack_image = SnackImage.find(params[:snack_id])
+    @snack_image = SnackImage.find(params[:id])
   end
 
+  def set_snack
+    @snack = Snack.find(params[:snack_id])
+  end
 end
