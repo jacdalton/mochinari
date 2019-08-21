b18f52b2221622691a708d512f9a70a00068e42c
Mitch Shiotani <mitch.shiotani1@gmail.com>
2019-08-21 19:19:22 +0900

    fixed avg stars method in case of no rating

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -11,8 +11,8 @@ class Snack < ApplicationRecord
   validates :category, presence: true
 
   def avg_snack_stars
-    snack_rating_array = Snack.find(id).snack_ratings
-    snack_stars_array = snack_rating_array.map(&:stars)
+    return 0 if snack_ratings.empty?
+    snack_stars_array = snack_ratings.map(&:stars)
     avg_stars_float = snack_stars_array.inject { |sum, el| sum + el }.to_f / snack_stars_array.size
     avg_stars_float.round(half: :up)
   end
