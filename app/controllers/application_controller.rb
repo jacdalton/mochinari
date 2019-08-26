19aec5dc8906cc6a873d7d8e0ad55130d0df4872
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-26 17:30:21 +0900

    Added devise parameters for user, and added geocode to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :about_me, :location])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :about_me, :location])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   acts_as_favoritor
+  geocoded_by :location
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :trails, dependent: :destroy
