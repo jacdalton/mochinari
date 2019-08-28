a5c118f6a2c663fa1a05b2877195b258a9aae311
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-28 19:58:30 +0900

    fixed merge

--- app/javascript/packs/user-loc.js
+++ app/javascript/packs/user-loc.js
@@ -1,59 +1,174 @@
-var map;
+const styles = [
+  {
+      "featureType": "all",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#d3c0d9"
+          }
+      ]
+  },
+  {
+      "featureType": "all",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "gamma": 0.01
+          },
+          {
+              "lightness": 20
+          }
+      ]
+  },
+  {
+      "featureType": "all",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+          {
+              "saturation": -31
+          },
+          {
+              "lightness": -33
+          },
+          {
+              "weight": 2
+          },
+          {
+              "gamma": 0.8
+          }
+      ]
+  },
+  {
+      "featureType": "all",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "landscape",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "lightness": 30
+          },
+          {
+              "saturation": 30
+          }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "saturation": 20
+          }
+      ]
+  },
+  {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "lightness": 20
+          },
+          {
+              "saturation": -20
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "lightness": 10
+          },
+          {
+              "saturation": -30
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "saturation": 25
+          },
+          {
+              "lightness": 25
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "all",
+      "stylers": [
+          {
+              "lightness": -20
+          }
+      ]
+  }
+];
 
 function initMap() {
-  const mapElement = document.getElementById('user-map');
-  const infoWindow = new google.maps.InfoWindow;
+const mapElement = document.getElementById('user-map');
+const infoWindow = new google.maps.InfoWindow;
 
-  if (mapElement) { // don't try to build a map if there's no div#map to inject in
-     map = new google.maps.Map(
-      mapElement,
-      {
-        zoom: 16,
-        center: {lat: 0, lng: 0}
-      }
-    );
-
-    const markers = JSON.parse(mapElement.dataset.markers);
-    const icon = mapElement.dataset.icon;
-    markers.forEach(marker => {
-      new google.maps.Marker(
-        {
-          position: marker,
-          map: map,
-          icon: icon
-        }
-      )
-    });
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('You are here');
-        infoWindow.open(map);
-        map.setCenter(pos);
-      }, function() {
-        console.log(map)
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+if (mapElement) { // don't try to build a map if there's no div#map to inject in
+  const map = new google.maps.Map(
+    mapElement,
+    {
+      zoom: 16,
+      center: {lat: 35.6804, lng: 139.7690},
+      styles: styles
     }
+  );
+
+console.log(map);
+  const markers = JSON.parse(mapElement.dataset.markers);
+  const icon = mapElement.dataset.icon;
+
+  markers.forEach(marker => {
+    new google.maps.Marker(
+      {
+        position: marker,
+        map: map,
+        icon: icon
+      }
+    )
+  });
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('You are here');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
   }
+}
 }
 
 initMap();
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
+infoWindow.setPosition(pos);
+infoWindow.setContent(browserHasGeolocation ?
+                      'Error: The Geolocation service failed.' :
+                      'Error: Your browser doesn\'t support geolocation.');
+infoWindow.open(map);
 }
-
-
