90f070283b5cfdfe1deb3eb940c4030b46d58e22
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-29 15:14:22 +0900

    hot garbage

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,12 +2,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   include CarrierWave::MiniMagick
 
-  process :rotate_according_to_exif
+  process :auto_orient
 
-  def rotate_according_to_exif
-    manipulate! do |img|
-      image = MiniMagick::Image.open(img)
-      image.tap(&:auto_orient)
-    end
+  def auto_orient 
+    manipulate! do |img| 
+      img.auto_orient!
+    end 
   end
 end
