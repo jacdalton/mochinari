e4f140d41e2a744b28b8bcdbbcb7961b133bf74d
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-29 20:33:52 +0900

    fixed em

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,38 +1,8 @@
+require 'rmagick'
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
-  include CarrierWave::MiniMagick
-  include CarrierWave::RMagick
 
- 
-  process :strip!
+  process eager: true
+  process cloudinary_transformation: { effect: :improve }
 
-
-  # version :stripped do 
-  #   process :strip, if: image?
-  # end
-
-  # def strip
-  #   manipulate! do |img|
-  #     img.strip!
-  #     img = yield(img) if block_given?
-  #     img
-  #   end
-  # end
-
-  # private 
-  # def image? 
-  #   true
-  # end
-
-  # process :remove_animation
-
-  # def remove_animation
-  #   manipulate! do |img|
-  #     if img.mime_type.match /gif/
-  #       img.collapse!
-  #     end
-  #     img.strip!
-  #     img
-  #   end
-  # end
 end
